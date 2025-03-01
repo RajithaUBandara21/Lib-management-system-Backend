7f76ec969eb9cb91f7c7a582d8bd8f04d3571c62
Rajitha Bandara <138967884+RajithaUBandara21@users.noreply.github.com>
2025-03-01 18:50:04 +0530

    created book database and aded lombok before add swager dependency

---
+++ src/main/java/com/example/library_management_system/dto/BookDTO.java
@@ -0,0 +1,19 @@
+package com.example.library_management_system.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BookDTO {
+    private String isbn;
+    private String title;
+    private String author;
+    private String category;
+    private int qty;
+}
---
+++ src/main/java/com/example/library_management_system/entity/Book.java
@@ -0,0 +1,35 @@
+package com.example.library_management_system.entity;
+
+
+import com.sun.jdi.Value;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @Column(name = "book_isbn" ,nullable = false  )
+   private String isbn;
+
+    @Column(name = "book_title" ,nullable = false  )
+    private String title;
+    @Column(name = "book_auther" ,nullable = false  )
+    private String author;
+    @Column(name = "book_category" ,nullable = false  )
+    private String category;
+    @Column(name = "book_qty" ,nullable = false  )
+    private int qty;
+
+
+}
